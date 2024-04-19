9aa57d78ffef25aed9439f9a8d399ec045cb36cd
a23gabli <a23gabli@student.his.se>
2024-04-19 16:13:43 +0200

    added data to intent bundle using extras and added a textview in SecondActivity to show data from intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("1", "test");
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,8 +1,9 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("1");
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText(data);
     }
 }
